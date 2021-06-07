e002fe607d1568c16b35fe680f4a36485ead75dd
Mukund.G <82136035+1NT19IS417@users.noreply.github.com>
2021-06-08 01:29:48 +0530

    Uploaded Program 3

---
+++ Map Reduce Programs/3. Salary.java
@@ -0,0 +1,66 @@
+// 3.Total number of awards for those with salary > 30000
+
+package my.bd.pack;
+
+import java.io.IOException;
+import java.util.*;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.conf.*;
+import org.apache.hadoop.io.*;
+import org.apache.hadoop.mapred.*;
+import org.apache.hadoop.util.*;
+
+public class salary 
+{
+	  //MAPPER CODE	
+
+	  public static class Map extends MapReduceBase implements Mapper < LongWritable, Text, Text, IntWritable > 
+	  {
+	    private final static IntWritable one = new IntWritable(1);
+	    private Text word = new Text();
+
+	    public void map(LongWritable key, Text value, OutputCollector < Text, IntWritable > output, Reporter reporter) throws IOException 
+	    {
+	      String myvalue = value.toString();
+	      String[] tokens = myvalue.split(",");
+
+	      Text award = new Text("Total awards won by employees with salary of 30000: ");
+			if (Integer.parseInt(tokens[2]) > 30000) 
+			{
+				output.collect(award, new IntWritable(Integer.parseInt(tokens[3])));
+			}
+	    }
+	  }
+
+	  //REDUCER CODE	
+	  public static class Reduce extends MapReduceBase implements Reducer < Text, IntWritable, Text, IntWritable > 
+	  {
+	    public void reduce(Text key, Iterator < IntWritable > values, OutputCollector < Text, IntWritable > output, Reporter reporter) throws IOException 
+	    { 
+	      int total = 0;
+	      while (values.hasNext()) 
+	      {
+	        total += values.next().get();
+	      }
+	      output.collect(key, new IntWritable(total));
+
+	    }
+	  }
+
+	  //DRIVER CODE
+	  public static void main(String[] args) throws Exception 
+	  {
+	    JobConf conf = new JobConf(salary.class);
+	    conf.setJobName("To Count the no of awards with Employees getting 30k as a salary");
+	    conf.setOutputKeyClass(Text.class);
+	    conf.setOutputValueClass(IntWritable.class);
+	    conf.setMapperClass(Map.class);
+	    conf.setCombinerClass(Reduce.class);
+	    conf.setReducerClass(Reduce.class);
+	    conf.setInputFormat(TextInputFormat.class);
+	    conf.setOutputFormat(TextOutputFormat.class);
+	    FileInputFormat.setInputPaths(conf, new Path(args[0]));
+	    FileOutputFormat.setOutputPath(conf, new Path(args[1]));
+	    JobClient.runJob(conf);
+	  }
+}
